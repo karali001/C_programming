b83c43ee4623d9c63f8ac800626cd32dbf6afe25
Enes KARALİ <38677441+karalienes@users.noreply.github.com>
2021-05-16 09:11:21 +0300

    New title update

---
+++ C/Lesson_4.c
@@ -0,0 +1,24 @@
+// ARITMETIK ISLEMLER-2
+
+/*
+
+(-x) ve (+) işlemlerı 1. oncelık sırası ve sagdan sola seklınde 
+*,/,% işlemleri 2. öncelik sırası ve soldan saga seklınde
++,- 3.öncelik sırası ve soldan saga seklınde
+parantez ıclerı her zaman en once yapılacaktır kısaca soyle demek daha dogrudur matematıkdekı ıslem oncelıgı ıle aynıdır :)
+
+*/
+
+
+#include<stdio.h> /*prıntf komutunun calısması ıcın yazılacaktır*/
+
+int main() {
+	
+	
+	
+	printf("%d\n",(-3+5)*(5-3)/(5%4));
+	
+		/*yazılımcılar ıslemdekı oncelıklerı daha cok karısık olmasın dıye parantez ıcınde kullanarak onlemektedır!*/
+	
+	return 0;/* bıtırme komutudur*/
+}
